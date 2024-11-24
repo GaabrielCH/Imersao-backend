7543ac23f6edf8fb6c715943a78a6decd226730d
Gabriel Camlofski Horst <gabrielchpessoal@gmail.com>
2024-11-24 02:07:34 -0300

    Aula 02 - Criando e Estruturando sua Primeira API GET e Banco de Dados

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ Instalar o Node.js;
 Aprender o que é um servidor;
 Criar e subir um servidor;
 Criar a sua API Key do Gemini.
+
+### Aula 02 :
+Criar uma base de dados;
+Criar um mock com ajuda do Gemini;
+Armazenar os seus dados;
+Adicionar rotas;
+Utilizar o bancos de dados MongoDB;
+Preparar o MongoDB para subir o recurso na Nuvem.
--- server.js
+++ server.js
@@ -1,11 +1,39 @@
 import express from 'express';
 
+const posts = [
+     {
+          id: 1,
+          descricao: 'Foto teste',
+          images: 'https://placecats.com/millie/300/150',
+
+     },
+     {
+          id: 2,
+          descricao: 'Foto teste 2',
+          images: 'https://placecats.com/millie/300/150',
+
+     },
+     {
+          id: 3,
+          descricao: 'Foto teste 3',
+          images: 'https://placecats.com/millie/300/150',
+
+     }
+];
+
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
      console.log('Server escutando..., porta 3000');
      });
 
-app.get('/api', (req, res) => {
-     res.status(200).send('API no ar!');
+function buscarPostPorId(id) {
+     return posts.findIndex((post) => 
+          post.id === Number(id));
+     }
+
+app.get('/posts/:id', (req, res) => {
+     const id = buscarPostPorId(req.params.id);
+     res.status(200).json(posts[id]);
      });
