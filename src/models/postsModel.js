8bc65d53a43f00446bf94362248c38908ab5f488
Gabriel Camlofski Horst <gabrielchpessoal@gmail.com>
2024-11-25 03:19:14 -0300

    Aula 05 - Publicando na Google Cloud: Configuração de API e Integração

--- src/models/postsModel.js
+++ src/models/postsModel.js
@@ -1,17 +1,23 @@
-import conectarAoBanco from '../config/dbConfig.js';
-
+import dotenv from "dotenv";
+import { ObjectId } from "mongodb";
+import conectarAoBanco from "../config/dbConfig.js"
 const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 
 export async function getTodosPosts() {
-     const db = conexao.db('imersao-instabytes');
-     const colecao = db.collection('posts');
-
-     return colecao.find().toArray();
-     }
+    const db = conexao.db("imersao-instabytes");
+    const colecao = db.collection("posts");
+    return colecao.find().toArray();
+}
 
 export async function criarPost(novoPost) {
-     const db = conexao.db('imersao-instabytes');
-     const colecao = db.collection('posts');
+    const db = conexao.db("imersao-instabytes");
+    const colecao = db.collection("posts");
+    return colecao.insertOne(novoPost);
+}
 
-     return colecao.insertOne(novoPost);
-     }
+export async function atualizarPost(id, novoPost) {
+    const db = conexao.db("imersao-instabytes");
+    const colecao = db.collection("posts");
+    const objID = ObjectId.createFromHexString(id);
+    return colecao.updateOne({_id: new ObjectId(objID)}, {$set:novoPost});
+}
