5d87f798ccdcb67917542e713a5cd646a8bac075
Gabriel Camlofski Horst <gabrielchpessoal@gmail.com>
2024-11-25 01:56:33 -0300

    Aula 04 - Implementando Armazenamento e Upload de Imagens

--- README.md
+++ README.md
@@ -1,23 +1,51 @@
 # Imersao backend da Alura
 
 ### Aula 01 :
+
 Conhecer o projeto de blog de fotos;
+
 Instalar o Node.js;
+
 Aprender o que é um servidor;
+
 Criar e subir um servidor;
+
 Criar a sua API Key do Gemini.
 
 ### Aula 02 :
+
 Criar uma base de dados;
+
 Criar um mock com ajuda do Gemini;
+
 Armazenar os seus dados;
+
 Adicionar rotas;
+
 Utilizar o bancos de dados MongoDB;
+
 Preparar o MongoDB para subir o recurso na Nuvem.
 
 ### Aula 03 :
+
 Configurar o primeiro Cluster, Database e coleção no MongoDB;
+
 Criar variáveis de ambiente;
+
 Conectar o Banco com a API via String de conexão;
+
 Criar código de configuração do banco na API;
+
 Refatoração do primeiro get.
+
+### Aula 04 :
+
+Enviar informações para a sua base de dados;
+
+Aprender sobre verbos HTTP;
+
+Criar a rota POST/post e POST/upload;
+
+Implementar o upload de imagens;
+
+Testar a API com thunderClient e Postman.
--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,7 +1,33 @@
-import getTodosPosts from "../models/postsModel.js";
+import {getTodosPosts, criarPost} from "../models/postsModel.js";
+import fs from 'fs';
 
-export default async function listarPosts(req, res) {
+export async function listarPosts(req, res) {
      const posts = await getTodosPosts();
      res.status(200).json(posts);
 }
 
+export async function postarNovoPost(req, res) {
+     const novoPost = req.body;
+     try{
+          const postCriado = await criarPost(novoPost);
+          res.status(200).json(postCriado);
+     }
+     catch (erro){
+          console.error(erro.message);
+          res.status(400).send('Erro ao criar post');
+     }
+}
+
+export async function uploadImagem(req, res) {
+     const novoPost = req.body;
+     try{
+          const postCriado = await criarPost(novoPost);
+          const arqAtualizado = `uploads/${postCriado.insertedId}.png`;
+          fs.renameSync(req.file.path, arqAtualizado);
+          res.status(200).json(postCriado);
+     }
+     catch (erro){
+          console.error(erro.message);
+          res.status(400).send('Erro ao criar post');
+     }
+}
